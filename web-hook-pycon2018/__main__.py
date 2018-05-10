f1ef37e6ca8a76a6c5022f51a09bb575fb070a9d
bellis-penguin <bellis@penguincomputing.com>
2018-05-10 11:11:06 -0400

    cleaning up code

--- web-hook-pycon2018/__main__.py
+++ web-hook-pycon2018/__main__.py
@@ -11,12 +11,12 @@ router = routing.Router()
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
     """ Whenever an issue is opened, greet the author and say thanks."""
-        url = event.data["issue"]["comments_url"]
-        author = event.data["issue"]["user"]["login"]
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
 
-        message = f"Thanks for the report @{author}! \
-                    I will look into it right away! (Also, I'm a bot)."
-        await gh.post(url, data={"body": message})
+    message = f"Thanks for the report @{author}! \
+                I will look into it right away! (Also, I'm a bot)."
+    await gh.post(url, data={"body": message})
 
 async def main(request):
     body = await request.read()
